708778a89bf2d4a9d28a966432387b1df2bb130d
aditya allam <aditya@codebrunch.dev>
2025-12-11 16:54:22 +0530

    Added UnAuthorizedException.java

---
+++ src/main/java/dev/codebrunch/productservice/exceptions/UnAuthorizedException.java
@@ -0,0 +1,9 @@
+package dev.codebrunch.productservice.exceptions;
+
+public class UnAuthorizedException extends RuntimeException {
+    private String message;
+
+    public UnAuthorizedException(String message) {
+        super(message);
+    }
+}
